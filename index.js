684850d940b1565cac87c1ca310d0bc235a878af
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-11-11 09:28:31 +0100

    register updated

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
     let registerObject = {}
     let loginObject = {}
-    let repository = [
-        {first_name: "kalu", last_name: "emmanuel", phone_number: "08154567898", password: "456Ugc"},
-        {first_name: "biodun", last_name: "prince", phone_number: "08154547198", password: "866Unc"},
-    ]
+    // let repository = [
+    //     {first_name: "kalu", last_name: "emmanuel", phone_number: "08154567898", password: "456Ugc"},
+    //     {first_name: "biodun", last_name: "prince", phone_number: "08154547198", password: "866Unc"},
+    // ]
 
     // button element
     function confirmRegisteration(){
@@ -39,7 +39,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
         registerObject = {...registerObject,[e.target.name]: e.target.value}
     }
 
-    function handleLogInCharge(e){
+    function handleLoginChange(e){
         loginObject = {...loginObject, [e.target.name]: e.target.value}
     }
 
@@ -59,7 +59,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
         let userRepository = JSON.parse(localStorage.getItem("ewausers"))
         let {email, password} = loginObject
         let userObject = userRepository.find(user => user.email === email)
-        if (userRepository.findIndex(user => user.emal === email) !== -1){
+        if (userRepository.findIndex(user => user.email === email) !== -1){
                 if (userObject.password === password){
                 localStorage.setItem("ewauser", JSON.stringify(userObject))
                 window.location.href = "home.html"
@@ -70,18 +70,29 @@ document.addEventListener("DOMContentLoaded", ()=>{
             alert('user does not exist')
         }
     }
-    if (document.getElementsByClassName('logiin_input'.length > 0))
+    if (document.getElementsByClassName('login_input').length > 0){
     let loginButton = document.getElementsByClassName('login_btn')[0]
     loginButton.addEventListener("click", () => confirmLogin())
 
+    }
+    //document.addEventListener("DOMContentLaoded", manipulator)
 
 
 
-    let inputFields = document.getElementsByTagName('input')
-        for (let i =0; i < inputFields.length; i++){
-            inputFields[i].addEventListener('input', (e) => handleInputChange(e))
+    // let inputFields = document.getElementsByTagName('input')
+    //     for (let i =0; i < inputFields.length; i++){
+    //         inputFields[i].addEventListener('input', (e) => handleInputChange(e))
 
-             //Array.from(inputFields).forEach((inputField) => inputField.addEventListener('input', (e) => handleInputChange(e)))
-        }
-    })
+    //          //Array.from(inputFields).forEach((inputField) => inputField.addEventListener('input', (e) => handleInputChange(e)))
+    //     }
+     })
+
+     function beansFunction(){
+        let user = JSON.parse(localStorage.getItem("ewauser "))
+
+        document.getElementById("user_name").innerHTML = "Hey" + " " + user.last_name;
+    
+     }
+
+     beansFunction()
 
